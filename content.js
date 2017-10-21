1c7c14e1fcc11cb8ea5badb92bf2d2fa403f340c
Emil Valeev <emil.musician@gmail.com>
2017-10-22 02:48:30 +0300

    Update content script
    Move click handler into separate function;
    Add getter for content from API response;
    Add functions for toggling button visibility;
    Move request logic into async functon.

--- content.js
+++ content.js
@@ -1,35 +1,85 @@
 "use strict";
 
-window.addEventListener('click', () => {
-  const selectedText = window.getSelection().toString();
+function getContentFromData (resp) {
+  return Object.values(resp.query.pages)[0].revisions[0]['*'];
+}
 
-  if (selectedText.length === 0) return;
+function showButtonOnCoords (x, y) {
+  const findButton = () => document.getElementById('wikiHighlightButton');
+  const buttonExist = findButton() !== null;
+  const button = buttonExist
+    ? findButton()
+    : document.createElement('button');
 
-  const params = {
+  button.style.left = `${x}px`;
+  button.style.top = `${y}px`;
+
+  if (button.style.display === 'none') button.style.display = 'block';
+
+  if (!buttonExist) {
+    button.id = 'wikiHighlightButton';
+    button.innerHTML = 'Show Wiki';
+    button.style.position = 'absolute';
+
+    document.body.appendChild(button);
+  }
+}
+
+function hideButton () {
+  const findButton = () => document.getElementById('wikiHighlightButton');
+  const buttonExist = findButton() !== null;
+
+  if (buttonExist) findButton().style.display = 'none';
+}
+
+function renderContent (content) {
+  console.log(content)
+}
+
+function fetchData (text) {
+  const apiUrl = new URL('https://en.wikipedia.org/w/api.php');
+  const userAgent = 'Wiki Highlight (https://github.com/emil14/wiki-highlight)';
+  const searchParams = {
     action: 'query',
     format: 'json',
     prop: 'revisions',
     rvprop: 'content',
     rvsection: 0,
-    titles: selectedText
+    titles: text
   };
-  const url = new URL('https://en.wikipedia.org/w/api.php');
 
-  Object.keys(params).forEach(key => {
-    url.searchParams.append(key, params[key]);
+  Object.keys(searchParams).forEach(key => {
+    apiUrl.searchParams.append(key, searchParams[key]);
   });
 
-  fetch(url, {
-    method: 'POST',
-    headers: new Headers({
-      'Api-User-Agent': 'Wiki-Highlight/0.0.1 (https://github.com/emil14/wiki-highlight)'
+  return new Promise((resolve, reject) => {
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: new Headers({ 'Api-User-Agent': userAgent })
     })
+      .then(resp => resp.json())
+      .then(data => {
+        resolve(data);
+      })
+      .catch(err => {
+        reject(err);
+      });
   })
-    .then(resp => resp.json())
+}
+
+function handleClickEvent (event) {
+  const selectedText = window.getSelection().toString();
+
+  if (selectedText.length === 0) {
+    hideButton();
+    return;
+  }
+
+  fetchData(selectedText)
     .then(data => {
-      console.log(data.query.pages);
-    })
-    .catch(err => {
-      console.log(err);
+      showButtonOnCoords(event.pageX, event.pageY);
+      renderContent(getContentFromData(data));
     });
-});
+}
+
+window.addEventListener('click', handleClickEvent);
