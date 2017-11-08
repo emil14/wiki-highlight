fb2caa09ad68cdeb47c4e8f8133d5644f0d0a095
Emil Valeev <emil.valeev@emlid.com>
2017-11-08 21:03:12 +0300

    popup: Refactor entry point with spread syntax

--- src/app_popup/index.js
+++ src/app_popup/index.js
@@ -1,14 +1,16 @@
+const { document, chrome } = window;
+
 let isEnabled = true;
-const switchButton = window.document.getElementById('switchButton');
+const switchButton = document.getElementById('switchButton');
 
 function toggleMode() {
   isEnabled = !isEnabled;
   const iconPath = isEnabled ? 'wiki-logo.png' : 'wiki-logo--disabled.png';
 
-  window.chrome.browserAction.setIcon({ path: iconPath });
+  chrome.browserAction.setIcon({ path: iconPath });
 
-  window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    window.chrome.tabs.sendMessage(tabs[0].id, isEnabled);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, isEnabled);
   });
 }
 
