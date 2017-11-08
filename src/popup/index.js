306ca82088b9732f22ba87650fb675883117e47c
Emil Valeev <emil.valeev@emlid.com>
2017-11-09 00:00:31 +0300

    popup: Store mode in localStorage; This closes #4

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,12 +1,21 @@
-const { document, chrome } = window;
-
-let isEnabled = true;
+const { document, chrome, localStorage } = window;
 const switchButton = document.getElementById('switchButton');
+const storageKey = 'wk.enabled';
+const getStoragedValue = () => localStorage.getItem(storageKey);
+const setStoragedValue = (value) => {
+  localStorage.setItem(storageKey, value);
+};
+const storageIsClear = getStoragedValue() === null;
+
+if (storageIsClear) setStoragedValue(true);
 
 function toggleMode() {
-  isEnabled = !isEnabled;
-  const iconPath = isEnabled ? './wiki-logo.png' : './wiki-logo--disabled.png';
+  let isEnabled = getStoragedValue();
 
+  isEnabled = !isEnabled;
+  localStorage.setItem(storageKey, isEnabled);
+
+  const iconPath = isEnabled ? './wiki-logo.png' : './wiki-logo--disabled.png';
   chrome.browserAction.setIcon({ path: iconPath });
 
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
