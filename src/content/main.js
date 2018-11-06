bca3d1c19e7a013f6b28f82e990e2d888db7b1ee
emil14 <emil.musician@gmail.com>
2018-11-06 21:06:36 +0300

    Fix bug with async message sending

--- src/content/main.js
+++ src/content/main.js
@@ -7,9 +7,10 @@ document.onselectionchange = () => {
 runtime.onMessage.addListener((request, _sender, sendResp) => {
   if (request.name !== 'toggle') return;
 
-  // FIXME async code doesn't work here
   storage.sync.get(['isEnabled'], ({ isEnabled }) => {
     const newVal = !isEnabled;
     storage.sync.set({ isEnabled: newVal }, () => sendResp({ isEnabled: newVal }));
   });
+
+  return true;
 });
